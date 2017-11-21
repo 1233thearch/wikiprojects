198a80417e25fb6d2e79e57e21e2c085617e90ac
William Chan <wc.1233.chan@gmail.com>
2017-11-21 20:54:08 +0800

    Add

---
+++ Codes/Chinese Wikipedia/Checkadminers.js
@@ -0,0 +1,145 @@
+﻿(function($, mw) {
+// Create portlet link
+var portletLinkOnline = mw.util.addPortletLink(
+    'p-personal',
+    '#',
+    '管理人員',
+    't-onlineadmin',
+    '查看当前在线管理员',
+    '',
+    '#pt-userpage'
+);
+    var rcstart, rcend, time;
+    var users = [];
+    var admins = [], rollbackers = [], patrollers = [];
+    var api = new mw.Api();
+
+    // Bind click handler
+    $(portletLinkOnline).click(function(e) {
+        e.preventDefault();
+
+        users = [];
+        var usersExt = [];
+        admins = [];
+        rollbackers = [];
+        patrollers = [];
+
+        // 最近更改30分钟内的编辑用户
+        time = new Date();
+        rcstart = time.toISOString();
+        time.setMinutes(time.getMinutes() - 30);
+        rcend = time.toISOString();
+
+        //API:RecentChanges
+        api.get({
+            format: 'json',
+            action: 'query',
+            list: 'recentchanges',
+            rcprop: 'user',
+            rcstart: rcstart,
+            rcend: rcend,
+            rcshow: '!bot|!anon',
+            rclimit: 500
+        }).done(function(data) {
+            $.each(data.query.recentchanges, function(i, item) {
+                users[i] = item.user;
+            });
+            api.get({
+                format: 'json',
+                action: 'query',
+                list: 'logevents',
+                leprop: 'user',
+                lestart: rcstart,
+                leend: rcend,
+                lelimit: 500
+            }).done(function(data) {
+                $.each(data.query.logevents, function(i, item) {
+                    usersExt[i] = item.user;
+                });
+
+                Array.prototype.push.apply(users, usersExt);
+
+                // 使用者名稱去重與分割
+                users = $.unique(users.sort());
+
+                var promises = [];
+                var mark = function(data) {
+                    $.each(data.query.users, function(i, user) {
+                        // 找到管理员，去除adminbot
+                        if ($.inArray('bot', user.groups) === -1) {
+                            if ($.inArray('sysop', user.groups) > -1) {
+                                admins[i] = user.name;
+                            }
+                            if ($.inArray('rollbacker', user.groups) > -1) {
+                                rollbackers[i] = user.name;
+                            }
+                            if ($.inArray('patroller', user.groups) > -1) {
+                                patrollers[i] = user.name;
+                            }
+                        }
+                    });
+                };
+                for (var i=0; i<(users.length+50)/50; i++) {
+                    promises.push(api.get({
+                        format: 'json',
+                        action: 'query',
+                        list: 'users',
+                        ususers: users.slice(i*50, (i+1)*50).join('|'),
+                        usprop: 'groups'
+                    }).done(mark));
+                }
+
+                // 查询用户权限
+                $.when.apply($, promises).done(function () {
+                    // 消除空值
+                    var filter = function(n) {
+                        return n;
+                    };
+                    
+                    admins = admins.filter(filter);
+                    rollbackers = rollbackers.filter(filter);
+                    patrollers = patrollers.filter(filter);
+
+                    var userlink = function(user) {
+                        var user2 = user.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&lt;');
+                        return '<br><a href="/wiki/User:' + user2 + '" target="_blank">' + user2 + '</a>&nbsp;<small style="opacity:.75;">(<a href="/wiki/User talk:' + user2 + '" target="_blank">留言</a>)</small>　';
+                    }
+                    
+                    if (admins.length + rollbackers.length + patrollers.length > 0) {
+                        var adminsstring = ['<p>當前在線管理人員</p>'];
+
+                        if (admins.length > 0) {
+                            adminsstring.push('<p style="word-break:break-all;">有' + admins.length + '個管理員在線：');
+                            $.each(admins, function(i, e) {
+                                adminsstring.push(userlink(e));
+                            });
+                            adminsstring.push('</p>');
+                        }
+
+                        if (patrollers.length > 0) {
+                            adminsstring.push('<p style="word-break:break-all;">有' + patrollers.length + '個巡查員在線：');
+                            $.each(patrollers, function(i, e) {
+                                adminsstring.push(userlink(e));
+                            });
+                            adminsstring.push('</p>');
+                        }
+
+                        if (rollbackers.length > 0) {
+                            adminsstring.push('<p style="word-break:break-all;">有' + rollbackers.length + '個回退員在線：');
+                            $.each(rollbackers, function(i, e) {
+                                adminsstring.push(userlink(e));
+                            });
+                            adminsstring.push('</p>');
+                        }
+
+                        mw.notify($(adminsstring.join('')));
+                    } else {
+                        mw.notify('現在沒有管理人員在線，可以去搞破壞了！');
+                    }
+                }).fail(function () {
+                    mw.notify('查詢時出現錯誤');
+                });
+            });
+        });
+    });
+})(jQuery, mw);
--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # Wikipedia Projects Backup Storage
-##Stores for backup use or for files that does not have a compatible license with Commons
-
-Storing some contributions to the wikipedia projects.
-
-// This is a place where Contributions to wikipedia (apart from edits of Wikipedia) of the owner of the GIT repository was placed in
-
-// This guy is NOT a genius so do expect a lot of bugs in some of the bugs.
-
-// Please note that it uses CC-BY-NC-SA license 4.0
+Stores MULTIPLE uses.
